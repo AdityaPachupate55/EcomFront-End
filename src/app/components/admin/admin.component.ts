2e5f4a2f1f5235cbcd6e38fbda456bb7bef35229
Aditya Pachupate <Aditya.Pachupate@cognizant.com>
2025-05-10 13:08:15 +0530

    Enhance admin dashboard with order statistics and low stock product retrieval , - removed header in admin dashboard

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -1,4 +1,3 @@
-
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "../../layout/header/header.component";
 import { Component, OnInit } from '@angular/core';
@@ -16,11 +15,15 @@ export class AdminComponent implements OnInit {
   totalProducts: number = 0;
   lowStockProducts: number = 0;
   products: any[] = [];
+  orders: any[] = [];
+  totalOrders: number = 0;
+  totalRevenue: number = 0;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit(): void {
     this.getProducts();
+    this.getOrders();
   }
 
   getProducts(): void {
@@ -37,6 +40,29 @@ export class AdminComponent implements OnInit {
 
   calculateTotals(): void {
     this.totalProducts = this.products.length;
-    this.lowStockProducts = this.products.filter(product => product.quantity < 10).length; // Assuming low stock is defined as quantity < 10
+    this.lowStockProducts = this.products.filter(product => product.quantity < 50).length; // Assuming low stock is defined as quantity < 10
   }
+
+  getLowStockProducts(): any[] {
+    return this.products.filter(product => product.quantity < 10);
+  }
+
+  getOrders(): void {
+    this.http.get<any[]>('https://localhost:7194/api/Orders').subscribe(
+      data => {
+        this.orders = data;
+        this.calculateOrderStats();
+      },
+      (error: HttpErrorResponse) => {
+        console.error('Error fetching orders:', error);
+      }
+    );
+  }
+
+  calculateOrderStats(): void {
+    this.totalOrders = this.orders.length;
+    this.totalRevenue = this.orders.reduce((sum, order) => sum + order.totalPrice, 0);
+  }
+
+  
 }
